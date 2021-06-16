3d8b74254077eec24cfd4c6cbcdb38e63c1a7d47
henryRiveraCS <44453113+henryriveraCS@users.noreply.github.com>
2021-06-16 18:34:00 -0400

    cleaned dijkstra file

--- algos/dijkstra.py
+++ algos/dijkstra.py
@@ -5,9 +5,7 @@ class Node:
 	def __init__(self, point):
 		self.point = point
 		self.cost = 1
-		self.distance = 100000
-		self.G = 0
-		self.H = 0
+		self.distance = float(math.inf)
 		self.parent = None
 	
 	def move_cost(self):
@@ -18,8 +16,9 @@ def neighbors(current_node, matrix, DIRECTION):
 		
 	if DIRECTION == 4:
 		neighbor_list = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
-	#else:	
-	#	neighbor_list = [(x+1, y+1), (x+1, y-1), (x-1, y+1), (x-1, y-1), (x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+	#any value other an 4 will assume the algo can travel in all 8 directions
+	else:	
+		neighbor_list = [(x+1, y+1), (x+1, y-1), (x-1, y+1), (x-1, y-1), (x-1, y), (x+1, y), (x, y-1), (x, y+1)]
 	
 	node_list = []
 	for neighbor in neighbor_list:
@@ -28,16 +27,6 @@ def neighbors(current_node, matrix, DIRECTION):
 				node_list.append(node)
 	
 	return node_list
-
-#find and return the tentative distance to the current node to the start node
-def dijkstra_distance(prev_node, current_node, start_node):
-	#because start node has a cost of 0 -> manually return 1 so it can add it to the neighboring node
-	if start_node:
-		return 0
-	else:
-		current_cost = prev_node.distance + current_node.move_cost
-		return current_cost
-
 
 def dijkstra(start_node, end_node, matrix, DIRECTION):
 	v_set = [] #visited nodes
@@ -50,7 +39,7 @@ def dijkstra(start_node, end_node, matrix, DIRECTION):
 	while current_node != end_node:
 		#remove current_node from U_set since it is going to be explroed now
 		#u_set.remove(current_node)
-		print("CURRENT NODE: ", current_node.point, current_node.distance)
+		#print("CURRENT NODE: ", current_node.point, current_node.distance)
 		neighbor_nodes = neighbors(current_node, matrix, DIRECTION)
 
 		#update the distance of every neighbor node to the appropiate cost
@@ -67,26 +56,27 @@ def dijkstra(start_node, end_node, matrix, DIRECTION):
 					for i in u_set:
 						if i.point == node.point:
 							i = node
-							print("UPDATED NODE: ", i.point, i.distance)
+							#print("UPDATED NODE: ", i.point, i.distance)
 
 		#now that all neighbor distances have been updated -> set current node as visited
 		v_set.append(current_node)
 		#now assign current_node to an unvisited node with minimal distance (from starting point)
 		lowest_cost_node = min(u_set, key=attrgetter("distance"))
 
-		print("LOWEST COST NODE: ", lowest_cost_node, lowest_cost_node.point, lowest_cost_node.distance)
+		#print("LOWEST COST NODE: ", lowest_cost_node, lowest_cost_node.point, lowest_cost_node.distance)
 		#DELETE THIS ONCE ALGO IS FINISHED
 		current_node = lowest_cost_node
 		u_set.remove(current_node)
-		print("NEWLY ASSIGNED CURRENT_NODE: ", current_node, current_node.point, current_node.distance)
+		#print("NEWLY ASSIGNED CURRENT_NODE: ", current_node, current_node.point, current_node.distance)
 
 	#this will run after the end node has been found
 	while current_node.parent:
 		print("PATH TO FINAL NODE: ", current_node, current_node.point, current_node.distance)
 		current_node = current_node.parent
 
+#START SCRIPT HERE
 start_point = (0, 0)
-end_point = (6, 4)
+end_point = (3, 5)
 DIRECTION = 4
 
 matrix = []
@@ -102,5 +92,4 @@ for node in matrix:
 	if node.point == end_point:
 		end_node = node
 
-#the first iteration of start node must be 0 (0 distance travelled)
 result = dijkstra(start_node, end_node, matrix, DIRECTION)
